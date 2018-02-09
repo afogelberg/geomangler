5f798d5e7efd30b58b4f8b7282c8082c00c884fb
foggis <andreas.fogelberg@vasteras.se>
2018-02-09 16:27:55 +0100

    Added gml support

--- geomangler.js
+++ geomangler.js
@@ -1,17 +1,26 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const XMLSerializer = require('./lib/utils/xml');
+const dom = new JSDOM().window;
 global.window = {};
+global.document = dom.document;
+global.Document = dom.Document;
+global.Node = dom.Node;
+global.DOMParser = dom.DOMParser;
+global.XMLSerializer = XMLSerializer;
 global.navigator = {
   userAgent: ''
 };
+
 const init = require('./lib/init');
 const manglers = require('./lib/manglers');
 const helpers = require('./lib/helpers');
-const file = require('./lib/file');
+const misc = require('./lib/misc');
 const server = require('./server/server');
 
 module.exports = function(opt_options) {
   const options = opt_options || {};
   init(options);
-  let geomangler = Object.assign({}, file, manglers, helpers);
-  geomangler.server = server;
+  let geomangler = Object.assign({}, misc, manglers, helpers);
   return geomangler;
 }();
